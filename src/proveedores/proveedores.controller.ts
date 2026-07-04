dd63ae3aad9d6ceec90f4103215363936e0be868
dmcalderonc97 <dmcalderonc97@gmail.com>
2026-07-04 08:42:49 -0500

    creacion de la rama en la nube

--- src/proveedores/proveedores.controller.ts
+++ src/proveedores/proveedores.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Put, Param, Delete, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, ParseIntPipe, Patch } from '@nestjs/common';
 import { ApiTags, ApiOperation, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
 
 import { ProveedoresService } from './proveedores.service';
